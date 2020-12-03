7345cedfdfa0545535bdbc71cb466cf15270acac
Sophon96 <71684640+Sophon96@users.noreply.github.com>
2020-12-03 08:58:55 -0800

    Removed some debug statements.

--- 2020/Day1P1.py
+++ 2020/Day1P1.py
@@ -1,11 +1,8 @@
 inp = open('../input/aoc-1.in', 'r').readlines()
 inp = [int(thing) for thing in inp]
-print(inp)
-# sum1 = int()
-# sum2 = int()
+
 for i in range(len(inp)):
 	for i2 in inp[i::]:
 		if inp[i] + i2 == 2020:
 			print(inp[i] * i2)
-			# print(inp[i], i2, i3)
 			exit(0)
--- 2020/Day3P1.py
+++ 2020/Day3P1.py
@@ -1,14 +1,11 @@
 inp = list(map(list, map(str.strip, open('../input/aoc-3.in', 'r').readlines())))
-print(inp)
-bottom = len(inp)
+
 width = len(inp[0])
-# print(width)
-# print(bottom)
 count = 0
+
 for i in inp[0::]:
-	# print(inp.index(i))
 	ind = (inp.index(i) * 3) % width
-	print(ind)
 	if i[ind] == '#':
 		count += 1
+
 print(count)
