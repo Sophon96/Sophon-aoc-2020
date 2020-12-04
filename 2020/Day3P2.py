01ca455486adbad65359b858c9aa8a87f3730e4b
Sophon96 <71684640+Sophon96@users.noreply.github.com>
2020-12-04 11:19:53 -0800

    Generalize tree1() to work with everything

--- 2020/Day3P2.py
+++ 2020/Day3P2.py
@@ -3,20 +3,11 @@ inp = list(map(list, map(str.strip, open('../input/aoc-3.in', 'r').readlines()))
 width = len(inp[0])
 
 
-def tree1(y):
+def tree1(y, x=1):
 	count = 0
-	for i in inp[0::]:
+	for i in inp[0::x]:
 		ind = (inp.index(i) * y) % width
-		if i[ind] == '#':
-			count += 1
-	return count
-
-
-def tree2():
-	count = 0
-	for i in range(0, len(inp), 2):
-		ind = (i / 2) % width
-		if inp[i][int(ind)] == '#':
+		if i[int(ind)] == '#':
 			count += 1
 	return count
 
@@ -27,6 +18,6 @@ c = tree1(5)
 d = tree1(7)
 # I used e back when I intended to use tree1() to do everything,
 # but that didn't work, so I used f and don't feel like changing it to e
-f = tree2()
+f = tree1(0.5, 2)
 total = a * b * c * d * f
 print(total)
